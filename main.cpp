b9160a561155332ba8a02834a317229c20832ba5
Nullbrine303 <46495743+Nullbrine303@users.noreply.github.com>
2026-07-22 15:38:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "RenderWare.h"
 #include "plugin.h"
+#include "RenderWare.h"
 #include "CAutomobile.h"
 #include "CTimer.h"
 
@@ -11,7 +11,6 @@ public:
         Events::initGameRegisterEvent [] {
             Events::vehicleRenderEvent += [](CVehicle* vehicle) {
                 if (vehicle && vehicle->m_pDriver == FindPlayerPed()) {
-                    // Yüksek FPS'te hassaslaşan/kilitlenen direksiyon açısını normalize eder
                     if (CTimer::ms_fTimeStep < 1.0f && CTimer::ms_fTimeStep > 0.0f) {
                         vehicle->m_fSteerAngle *= (1.0f / CTimer::ms_fTimeStep) * 0.85f;
                     }
